4e7b2c8b7fce771e3a2cb55135262306dad6e152
Rafael Lossurdo <lossurdo@gmail.com>
2016-01-02 16:55:11 -0200

    corrigida validação de campos obrigatórios

--- seriado-model/src/main/java/com/senac/seriadomodel/crud/RNException.java
+++ seriado-model/src/main/java/com/senac/seriadomodel/crud/RNException.java
@@ -3,6 +3,10 @@ package com.senac.seriadomodel.crud;
 public class RNException extends RuntimeException {
     private Tipo tipo;
 
+    public RNException(Tipo tipo) {
+        super(tipo.toString());
+        this.tipo = tipo;
+    }
     public RNException(Tipo tipo, String message) {
         super(message);
         this.tipo = tipo;
@@ -27,7 +31,20 @@ public class RNException extends RuntimeException {
      * relacionado às regras de negócio.
      */
     public enum Tipo {
-        REGISTRO_JA_EXISTE, 
-        REGISTRO_NAO_ENCONTRADO
+        REGISTRO_JA_EXISTE("Registro já existente"), 
+        REGISTRO_NAO_ENCONTRADO("Registro não encontrado"),
+        CAMPOS_OBRIGATORIOS_VIOLADOS("Campos obrigatórios não foram informados")
+        ;
+        
+        private String value;
+        
+        private Tipo(String mensagem) {
+            this.value = mensagem;
+        }
+        
+        @Override        
+        public String toString() {
+            return this.value;
+        }
     }
 }
--- seriado-model/src/main/java/com/senac/seriadomodel/rn/GeneroRN.java
+++ seriado-model/src/main/java/com/senac/seriadomodel/rn/GeneroRN.java
@@ -3,7 +3,9 @@ package com.senac.seriadomodel.rn;
 import com.senac.seriadomodel.bd.GeneroBD;
 import com.senac.seriadomodel.bean.Genero;
 import com.senac.seriadomodel.crud.CrudGenericoRN;
+import com.senac.seriadomodel.crud.RNException;
 import java.util.List;
+import org.apache.commons.lang.StringUtils;
 
 /**
  *
@@ -31,12 +33,22 @@ public class GeneroRN extends CrudGenericoRN<Genero> {
     @Override
     public Genero salvar(Genero bean) {
         avaliarSalvar(crudBD, bean);
+        
+        if(StringUtils.isBlank(bean.getGenero())) {
+            throw new RNException(RNException.Tipo.CAMPOS_OBRIGATORIOS_VIOLADOS);
+        }
+        
         return crudBD.salvar(bean);
     }
 
     @Override
     public Genero alterar(Genero bean) {
         avaliarAlterar(crudBD, bean);
+
+        if(StringUtils.isBlank(bean.getGenero())) {
+            throw new RNException(RNException.Tipo.CAMPOS_OBRIGATORIOS_VIOLADOS);
+        }
+
         return crudBD.alterar(bean);
     }
 
