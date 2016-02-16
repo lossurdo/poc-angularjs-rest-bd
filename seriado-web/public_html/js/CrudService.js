184ad64bb2972593aaa0949b635eac3e8da199de
Rafael Lossurdo <lossurdo@gmail.com>
2016-02-16 16:37:17 -0200

    Melhoria na exibição das mensagens na tela

--- seriado-web/public_html/js/CrudService.js
+++ seriado-web/public_html/js/CrudService.js
@@ -7,7 +7,7 @@
 angular.module('CrudServiceModule', [])
 
 .service('CrudService', ['$http', function($http) {
-            
+                        
     this.salvar = function(serviceURL, jsonObject, fn) {
         console.log(jsonObject);
         
@@ -18,34 +18,39 @@ angular.module('CrudServiceModule', [])
             headers: {
                 'Content-Type': 'application/json'
             }
-        }).success(function (data, status, headers, config, statusText) {            
-            fn(headers('Location'));
-        }).error(function (data, status, header, config, statusText) {
-            console.log('Erro: ' + statusText);
-        });        
+        })
+        .then(function successCallback(response) {
+            console.log(response);
+            fn(response.headers('Location'));
+        }, function errorCallback(response) {
+            console.log(response);
+            geraMensagem("Ooops! " + (response.statusText===""?OOPS:response.statusText));
+        });
     }
     
     this.consultar = function(serviceURL, id, fn) {
         console.log(id);
+        
         $http.get(serviceURL + "/" + id)
-            .success(function (data) {
-                console.log(data);
-                fn(data);
-            })
-            .error(function (data, status, header, config, statusText) {
-                console.log('Erro: ' + statusText);
+            .then(function successCallback(response) {
+                console.log(response);
+                fn(response.data);
+            }, function errorCallback(response) {
+                console.log(response);
+                geraMensagem("Ooops! " + (response.statusText===""?OOPS:response.statusText));
             });
     }
     
     this.excluir = function(serviceURL, id, fn) {
         console.log(id);
+        
         $http.delete(serviceURL + "/" + id)
-            .success(function (data) {
-                console.log(data);
+            .then(function successCallback(response) {                
+                console.log(response);
                 fn();
-            })
-            .error(function (data, status, header, config, statusText) {
-                console.log('Erro: ' + statusText);
+            }, function errorCallback(response) {
+                console.log(response);
+                geraMensagem("Ooops! " + (response.statusText===""?OOPS:response.statusText));
             });
     }
     
@@ -59,26 +64,41 @@ angular.module('CrudServiceModule', [])
             headers: {
                 'Content-Type': 'application/json'
             }
-        }).success(function (data, status, headers, config, statusText) {            
-            fn(headers('Location'));
-        }).error(function (data, status, header, config, statusText) {
-            console.log('Erro: ' + statusText);
-        });        
+        })
+        .then(function successCallback(response) {
+            console.log(response);
+            fn(response.headers('Location'));
+        }, function errorCallback(response) {
+            console.log(response);
+            geraMensagem("Ooops! " + (response.statusText===""?OOPS:response.statusText));
+        });
     }
 
     this.pesquisar = function(serviceURL, value, fn) {
         console.log(value);
 
         $http.get(serviceURL + "?q=" + value)
-            .success(function (data) {
-                console.log(data);
-                fn(data);
+            .then(function successCallback(response) {
+                console.log(response);
+                fn(response.data);
+            }, function errorCallback(response) {
+                console.log(response);
+                geraMensagem("Ooops! " + (response.statusText===""?OOPS:response.statusText));
             });
-    }
+    };
         
     // definição de mensagens padrões para ações de CRUD - abertura do modal
     this.geraMensagemDefault = function() {
+        $('#myModalMessage').text("Ação executada com sucesso!");
         $('#myModal').modal();        
     };    
-    
+
+    var geraMensagem = function(mensagem) {
+        $('#myModalMessage').text(mensagem);
+        $('#myModal').modal();        
+    };    
+
+        // http://www.mikezilla.com/exp0012.html
+    var OOPS = "\u00AF\\_(\u30C4)_/\u00AF";
+        
 }]);
