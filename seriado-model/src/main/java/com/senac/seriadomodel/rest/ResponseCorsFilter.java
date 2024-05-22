99b0de757e92f4a4aada3a9c6c0676dec78193e2
Rafael Lossurdo <lossurdo@gmail.com>
2024-05-22 09:57:06 -0300

    fix: Vulnerabilidades

--- seriado-model/src/main/java/com/senac/seriadomodel/rest/ResponseCorsFilter.java
+++ seriado-model/src/main/java/com/senac/seriadomodel/rest/ResponseCorsFilter.java
@@ -1,11 +1,12 @@
 package com.senac.seriadomodel.rest;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
-import org.apache.log4j.Logger;
+
 
 /**
  *
@@ -14,11 +15,11 @@ import org.apache.log4j.Logger;
 @Provider
 public class ResponseCorsFilter implements ContainerResponseFilter {
 
-    private static final Logger logger = Logger.getLogger(ResponseCorsFilter.class);
+    private static final Logger logger = Logger.getLogger(ResponseCorsFilter.class.getName());
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        logger.debug("Ativando filtro CORS para chamadas REST");
+        logger.info("Ativando filtro CORS para chamadas REST");
 
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -30,7 +31,7 @@ public class ResponseCorsFilter implements ContainerResponseFilter {
             responseContext.getHeaders().add("Access-Control-Allow-Headers", headers);
         }
         
-        logger.debug("HEADER final configurado: " + responseContext.getHeaders());
+        logger.info("HEADER final configurado: " + responseContext.getHeaders());
     }
 
 }
